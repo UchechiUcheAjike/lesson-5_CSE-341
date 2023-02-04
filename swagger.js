9d242563faaddc242d27bc3e979f36b769495f89
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2023-02-04 09:22:22 -0700

    changed from http to https

--- swagger.js
+++ swagger.js
@@ -7,7 +7,7 @@ const doc = {
     },
     host: 'lesson5-cse-341.onrender.com',
     // host: 'localhost:8080',
-    schemes: ['http']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
